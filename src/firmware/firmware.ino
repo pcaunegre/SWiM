9d1d22d7d90c12f6d3e0321dd8c4fb295574a4cb
Pascal <pascal.caunegre@free.fr>
2021-06-14 18:34:48 +0200

    depot creation

---
+++ src/firmware/firmware.ino
@@ -0,0 +1,13 @@
+/*
+
+Sigfox Wind Meter 
+based on Peet Bros. (PRO) anemometer
+
+Read the wind speed and direction interrupts from two input pins, compute wind
+Send through Sigfox
+
+Run on MKRFox1200
+
+*/
+
+
